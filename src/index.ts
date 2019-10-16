73afeba3b2bc1be3918c3bbff7ebe04cf6286c7a
Joris Noordermeer <joris@noordermeer.ch>
2019-10-16 20:21:07 +0200

    Add packages, start asking questions and store to configstore

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import {Command, flags} from '@oclif/command';
+import cli from 'cli-ux';
+import * as inquirer from 'inquirer'
+import * as Configstore from 'configstore';
 import {fileExists} from './files';
 
 class ActualImportCsv extends Command {
@@ -15,11 +18,16 @@ class ActualImportCsv extends Command {
         required: true,
     }];
 
+    config: any;
+
     async run() {
         const {args, flags} = this.parse(ActualImportCsv);
 
         // Check if file exists
-        this.checkIfFileExists(args.file);
+        // this.checkIfFileExists(args.file);
+
+        // Init Configstore
+        this.config = new Configstore('actual-import-csv');
 
         // Future: Select which format the file is in,
         // or detect automatically
@@ -29,6 +37,7 @@ class ActualImportCsv extends Command {
         // Transform the contents to an array holding the transactions
 
         // Enter Actual Budget
+        await this.askForActualBudget();
 
         // Choose Actual account to import to
 
@@ -47,6 +56,17 @@ class ActualImportCsv extends Command {
         } catch(err) {
             this.error(err)
         }
+    };
+
+    async askForActualBudget() {
+        const defaultStash = this.config.get('budgetId') || 'My-Stash';
+        const response = await inquirer.prompt([{
+            name: 'budgetId',
+            message: 'Please enter the Budget ID (Located in the settings under "Advanced")',
+            default: defaultStash
+        }]);
+        this.config.set('budgetId', response.budgetId);
+        this.log(response.budgetId);
     };
 
 }
