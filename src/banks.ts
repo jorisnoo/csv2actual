42618f76ec19a5cdcd0cc42c10ba8d8d88e2733e
Simon VDB <simonvdbroeck@gmail.com>
2021-01-13 14:54:55 +0100

    feat/add-support-for-fintro-csv-format

--- src/banks.ts
+++ src/banks.ts
@@ -1,10 +1,12 @@
 import {BelfiusBe} from './banks/belfius-be';
+import {FintroBe} from './banks/fintro-be';
 import {NeonEn} from './banks/neon-en';
 import {SparkasseDe} from './banks/sparkasse-de';
 import {ZkbDe} from './banks/zkb-de';
 
 export const supportedBanks = {
     'belfius-be': BelfiusBe,
+    'fintro-be': FintroBe,
     'sparkasse-de': SparkasseDe,
     'zkb-de': ZkbDe,
     'neon-en': NeonEn,
---
+++ src/banks/fintro-be.ts
@@ -0,0 +1,42 @@
+import * as moment from 'moment';
+
+import {Bank} from './base';
+
+interface FintroTransaction {
+  volgnummer: string; // format: 2021-XXXX
+  uitvoeringsdatum:string;
+  valutadatum: string;
+  bedrag: string;
+  'valuta-rekening': string;
+  'tegenpartij-van-de-verrichting': string;
+  details: string;
+  rekeningnummer: string;
+}
+
+export class FintroBe extends Bank {
+    static description = 'Fintro';
+
+    static requiredHeaders = [
+        'volgnummer', 'uitvoeringsdatum', 'bedrag', 'tegenpartij-van-de-verrichting'
+    ];
+
+    static transformTransactions(transactions: FintroTransaction[]) {
+        return transactions
+            // Remove transactions without a full reference number (some still have 2021-) as number
+            .filter(transaction => transaction.volgnummer.length > 6)
+            .map(obj => {
+                // Calculate amount
+                const amount = parseFloat(obj.bedrag.replace(',', '.')) * 100;
+
+                // Read date as utc (w/out timezone) and convert to js date
+                const date = moment.utc(obj.uitvoeringsdatum, 'DD/MM/YYYY').format('YYYY-MM-DD');
+
+                return {
+                    imported_id: obj.volgnummer,
+                    imported_payee: obj['tegenpartij-van-de-verrichting'],
+                    date,
+                    amount,
+                };
+            });
+    }
+}
