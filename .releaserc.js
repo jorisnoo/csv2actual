34c3dceae04fcfe45267cbb0c3908de921bf5e5c
Joris Noordermeer <joris@noordermeer.ch>
2021-05-09 11:19:42 +0200

    build: Adds changelog

--- .releaserc.js
+++ .releaserc.js
@@ -2,8 +2,9 @@ module.exports = {
     branches: ['main'],
     tagFormat: '${version}',
     plugins: [
-        ['@semantic-release/commit-analyzer'],
-        ['@semantic-release/release-notes-generator'],
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
         '@semantic-release/github',
         '@semantic-release/npm',
     ],
